ed7ac1ce6da8d4db85f4242d80b05d97c1050a98
cailudys <cailudys@gmail.com>
2023-04-18 19:01:12 +0800

    保存代码

---
+++ src/03_链表结构LinkedList/12_反转链表(使用循环来做).ts
@@ -0,0 +1,9 @@
+import ListNode from "./面试题ListNode";
+
+function reverseList(head: ListNode | null): ListNode | null {
+  if (head === null || head.next === null) return head;
+
+  return null;
+}
+
+export {};
