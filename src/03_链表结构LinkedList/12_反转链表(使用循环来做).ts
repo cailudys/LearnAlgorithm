c863edc239122f310846d1d6cd4b99948312e185
cailudys <cailudys@gmail.com>
2023-04-19 10:45:04 +0800

    反转链表

--- src/03_链表结构LinkedList/12_反转链表(使用循环来做).ts
+++ src/03_链表结构LinkedList/12_反转链表(使用循环来做).ts
@@ -3,7 +3,25 @@ import ListNode from "./面试题ListNode";
 function reverseList(head: ListNode | null): ListNode | null {
   if (head === null || head.next === null) return head;
 
-  return null;
+  let newHead = null;
+  let current: ListNode | null = head;
+
+  while (head) {
+    current = current?.next ?? null;
+    head.next = newHead;
+    newHead = head;
+    head = current;
+  }
+
+  return newHead;
 }
+
+// 模拟数据
+const node1 = new ListNode(1);
+node1.next = new ListNode(2);
+node1.next.next = new ListNode(3);
+console.log(node1);
+const newHead = reverseList(node1);
+console.log("newHead", newHead);
 
 export {};
