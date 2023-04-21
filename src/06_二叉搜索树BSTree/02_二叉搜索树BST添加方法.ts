26837a88df2a098e098ae22c3b073aa319391696
cailudys <cailudys@gmail.com>
2023-04-21 13:23:19 +0800

    实现二叉搜索树的方法

--- src/06_二叉搜索树BSTree/02_二叉搜索树BST添加方法.ts
+++ src/06_二叉搜索树BSTree/02_二叉搜索树BST添加方法.ts
@@ -1,1 +1,29 @@
+import Node from "../types/INode";
+import { btPrint } from "hy-algokit";
 
+// 实现树的节点类
+class TreeNode<T> extends Node<T> {
+  left: TreeNode<T> | null = null;
+  right: TreeNode<T> | null = null;
+}
+
+interface IBSTree<T> {
+  insert(value: T): T;
+  search(value: T): boolean;
+  min(): T;
+  max(): T;
+  inOrderTraverse(): void;
+  preOrderTraverse(): void;
+  postOrderTraverse(): void;
+  levelOrderTraverse(): void;
+  remove(value: T): boolean;
+}
+
+// 实现树的类
+class BSTree<T> implements IBSTree<T> {
+  private root: TreeNode<T> | null = null;
+}
+
+const bst = new BSTree();
+
+export default BSTree;
